b77ee3e4429cea80317b72f32293d9ae5f50e054
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-01-12 00:05:54 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './components/Content';
+import Sidebar from './components/Sidebar';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <div>
+          <Sidebar />
+          <Content />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Sidebar extends Component {
+  render() {
+    return (
+      <>
+        <h4>
+          <Link to="/">Login</Link>
+        </h4>
+        <nav>
+          <Link to="/search">Search</Link>
+          <Link to="/album/:id">Album</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/profile/edit">ProfileEdit</Link>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Sidebar;
