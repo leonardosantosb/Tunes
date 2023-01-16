80d4d25dae155d5b8805c3bf6f76ed434e12094e
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-01-16 12:45:42 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameUser: '',
+      loadingHeader: true,
+    };
+  }
+
+  componentDidMount() {
+    this.userName();
+  }
+
+  userName = async () => {
+    const name = await getUser();
+    this.setState({
+      loadingHeader: false,
+      nameUser: name.name,
+    });
+  };
+
   render() {
+    const { nameUser, loadingHeader } = this.state;
+    if (loadingHeader) {
+      return (<Loading />);
+    }
     return (
-      <div data-testid="header-component">Header</div>
+      <header data-testid="header-component">
+        <div data-testid="header-user-name">
+          {nameUser}
+        </div>
+      </header>
     );
   }
 }
