4277894e37edadbbb8470c679cb658cb74687f6d
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-01-19 14:24:21 -0300

    requisito 8

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  state = {
+    loading: false,
+    favorito: false,
+  };
+
+  async componentDidMount() {
+    this.setState({
+      favorito: await this.favoritemusic(),
+    });
+  }
+
+  handleChange = async ({ target: { checked } }) => {
+    const { musica } = this.props;
+    this.setState({
+      loading: true,
+    });
+    if (checked) {
+      await addSong(musica);
+    } else {
+      await removeSong(musica);
+    }
+    this.setState({
+      loading: false,
+      favorito: checked,
+    });
+  };
+
+  favoritemusic = async () => {
+    const { musica: { trackName } } = this.props;
+    const favoritas = await getFavoriteSongs();
+    return (
+      favoritas.map((favorita) => favorita.trackName).includes(trackName)
+    );
+  };
+
   render() {
     const { musica } = this.props;
-
+    const { loading, favorito } = this.state;
+    if (loading) return (<Loading />);
     return (
       <div>
         <p>{musica.trackName}</p>
@@ -16,6 +54,16 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorite">
+          Favorita
+          <input
+            type="checkbox"
+            id="favorite"
+            data-testid={ `checkbox-music-${musica.trackId}` }
+            onChange={ this.handleChange }
+            checked={ favorito }
+          />
+        </label>
       </div>
     );
   }
